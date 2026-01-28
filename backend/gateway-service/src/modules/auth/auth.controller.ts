f10f7ac68946db01332665efcc9b38c534428c77
Yakov Lavrinov <lawrinov.yackov@yandex.ru>
2026-01-28 22:34:24 +0300

    feat: add swagger documentation for auth controller and dto

--- backend/gateway-service/src/modules/auth/auth.controller.ts
+++ backend/gateway-service/src/modules/auth/auth.controller.ts
@@ -1,9 +1,15 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common'
+import { ApiOperation } from '@nestjs/swagger'
 
 import { SendOtpRequest } from './dto'
 
 @Controller('auth')
 export class AuthController {
+	@ApiOperation({
+		summary: 'Send otp code',
+		description:
+			'Send a verification code to the user phone number or email.'
+	})
 	@Post('otp/send')
 	@HttpCode(HttpStatus.OK)
 	public sendOtp(@Body() dto: SendOtpRequest) {
--- backend/gateway-service/src/modules/auth/dto/requests/send-otp.request.ts
+++ backend/gateway-service/src/modules/auth/dto/requests/send-otp.request.ts
@@ -1,11 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { IsEnum, IsString, Validate } from 'class-validator'
 import { IdentifierValidator } from 'src/shared/validators'
 
 export class SendOtpRequest {
+	@ApiProperty({
+		example: '+79008006050'
+	})
 	@IsString()
 	@Validate(IdentifierValidator)
 	public identifier: string
 
+	@ApiProperty({
+		example: 'phone',
+		enum: ['phone', 'email']
+	})
 	@IsEnum(['phone', 'email'])
 	public type: 'phone' | 'email'
 }
