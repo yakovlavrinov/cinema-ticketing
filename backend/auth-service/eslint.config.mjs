4ea67fe8d26fb75597b0ef582b424fd8f62aec88
Yakov Lavrinov <lawrinov.yackov@yandex.ru>
2026-01-30 19:42:41 +0300

    feat: install prettier setting

--- backend/auth-service/eslint.config.mjs
+++ backend/auth-service/eslint.config.mjs
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs'],
+    ignores: ['eslint.config.mjs', 'prettier.config.mjs'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
